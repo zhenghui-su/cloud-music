708ff5bd1514f60b09a88cb8295f5d0ef33e34b9
suzhenghui <343196323@qq.com>
2023-11-02 00:34:45 +0800

    初始化项目

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { GlobalStyle } from './style';
+import { renderRoutes } from 'react-router-config';//renderRoutes 读取路由配置转化为 Route 标签
+import { IconStyle } from './assets/iconfont/iconfont';
+import store from './store';
+import routes from './routes/index.js';
+import { HashRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
+
+  )
+}
+
+export default App;
