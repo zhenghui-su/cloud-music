5e6282e1b8cf66df7651a1a5c1b5ba9a4d8175c2
suzhenghui <343196323@qq.com>
2023-11-03 23:14:38 +0800

    下拉上拉加载，图片懒加载优化

--- src/baseUI/scroll/index.js
+++ src/baseUI/scroll/index.js
@@ -1,21 +1,41 @@
-import React, { forwardRef, useState, useEffect, useRef, useImperativeHandle } from "react"
+import React, { forwardRef, useMemo, useState, useEffect, useRef, useImperativeHandle } from "react"
 import PropTypes from "prop-types"
 import BScroll from "better-scroll"
 import styled from 'styled-components';
+import Loading from '../loading/index';
+import LoadingV2 from '../loading-v2/index';
+import { debounce } from "../../api/utils";
 
 const ScrollContainer = styled.div`
   width: 100%;
   height: 100%;
   overflow: hidden;
-`
+`;
+
+const PullUpLoading = styled.div`
+  position: absolute;
+  left:0; right:0;
+  bottom: 5px;
+  width: 60px;
+  height: 60px;
+  margin: auto;
+  z-index: 100;
+`;
+
+export const PullDownLoading = styled.div`
+  position: absolute;
+  left:0; right:0;
+  top: 0px;
+  height: 30px;
+  margin: auto;
+  z-index: 100;
+`;
+
 const Scroll = forwardRef((props, ref) => {
     const [bScroll, setBScroll] = useState();
-
     const scrollContaninerRef = useRef();
-
     const { direction, click, refresh, bounceTop, bounceBottom } = props;
-
-    const { pullUp, pullDown, onScroll } = props;
+    const { pullUp, pullDown, onScroll, pullUpLoading, pullDownLoading } = props;
 
     useEffect(() => {
         const scroll = new BScroll(scrollContaninerRef.current, {
@@ -92,13 +112,62 @@ const Scroll = forwardRef((props, ref) => {
         }
     }));
 
+    let pullUpDebounce = useMemo(() => {
+        return debounce(pullUp, 300)
+    }, [pullUp]);
+    // 千万注意，这里不能省略依赖，
+    // 不然拿到的始终是第一次 pullUp 函数的引用，相应的闭包作用域变量都是第一次的，产生闭包陷阱。下同。
 
+    let pullDownDebounce = useMemo(() => {
+        return debounce(pullDown, 300)
+    }, [pullDown]);
+    //...
+    // 之后直接调用 useMemo 返回的函数
+    // 滑动到底部
+    useEffect(() => {
+        if (!bScroll || !pullUp) return;
+        const handlePullUp = () => {
+            //判断是否滑动到了底部
+            if (bScroll.y <= bScroll.maxScrollY + 100) {
+                pullUpDebounce();
+            }
+        };
+        bScroll.on('scrollEnd', handlePullUp);
+        // 解绑
+        return () => {
+            bScroll.off('scrollEnd', handlePullUp);
+        }
+    }, [pullUp, pullUpDebounce, bScroll]);
+
+    // 判断用户的下拉动作
+    useEffect(() => {
+        if (!bScroll || !pullDown) return;
+        const handlePullDown = (pos) => {
+            //判断用户的下拉动作
+            if (pos.y > 50) {
+                pullDownDebounce();
+            }
+        };
+        bScroll.on('touchEnd', handlePullDown);
+        return () => {
+            bScroll.off('touchEnd', handlePullDown);
+        }
+    }, [pullDown, pullDownDebounce, bScroll]);
+
+    const PullUpdisplayStyle = pullUpLoading ? { display: "" } : { display: "none" };
+    const PullDowndisplayStyle = pullDownLoading ? { display: "" } : { display: "none" };
     return (
         <ScrollContainer ref={scrollContaninerRef}>
             {props.children}
+            {/* 滑到底部加载动画 */}
+            <PullUpLoading style={PullUpdisplayStyle}><Loading></Loading></PullUpLoading>
+            {/* 顶部下拉刷新动画 */}
+            <PullDownLoading style={PullDowndisplayStyle}><LoadingV2></LoadingV2></PullDownLoading>
         </ScrollContainer>
     );
 })
+
+
 
 Scroll.defaultProps = {
     direction: "vertical",
