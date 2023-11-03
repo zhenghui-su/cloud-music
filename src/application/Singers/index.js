8f930901c8bd741c1346145fcabe43f38fcb3597
suzhenghui <343196323@qq.com>
2023-11-03 22:14:21 +0800

    增加歌手模板横向滚动条

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Horizen from '../../baseUI/horizen-item'
+import { categoryTypes, alphaTypes } from '../../api/config'
+import { NavContainer } from './style'
 
-function Singers(props) {
+function Singers() {
+    let [category, setCategory] = useState('');
+    let [alpha, setAlpha] = useState('');
+
+    let handleUpdateCategory = (val) => {
+        setCategory(val);
+    }
+
+    let handleUpdateAlpha = (val) => {
+        setAlpha(val);
+    }
+
     return (
-        <div>Singers</div>
+        <NavContainer>
+            <Horizen
+                list={categoryTypes}
+                title={"分类 (默认热门):"}
+                handleClick={handleUpdateCategory}
+                oldVal={category}></Horizen>
+            <Horizen
+                list={alphaTypes}
+                title={"首字母:"}
+                handleClick={handleUpdateAlpha}
+                oldVal={alpha}></Horizen>
+        </NavContainer>
     )
 }
 
---
+++ src/baseUI/horizen-item/index.js
@@ -0,0 +1,93 @@
+import React, { useRef, useEffect, memo } from 'react';
+import styled from 'styled-components';
+import Scroll from '../scroll/index'
+import { PropTypes } from 'prop-types';
+import style from '../../assets/global-style';
+
+function Horizen(props) {
+    // 加入声明
+    const Category = useRef(null);
+
+    // 加入初始化内容宽度的逻辑
+    useEffect(() => {
+        let categoryDOM = Category.current;
+        let tagElems = categoryDOM.querySelectorAll("span");
+        let totalWidth = 0;
+        Array.from(tagElems).forEach(ele => {
+            totalWidth += ele.offsetWidth;
+        });
+        categoryDOM.style.width = `${totalWidth}px`;
+    }, []);
+
+    const { list, oldVal, title } = props;
+    const { handleClick } = props;
+    return (
+        <Scroll direction={"horizental"}>
+            <div ref={Category}>
+                <List>
+                    <span>{title}</span>
+                    {
+                        list.map((item) => {
+                            return (
+                                <ListItem
+                                    key={item.id}
+                                    className={`${oldVal === item.key ? 'selected' : ''}`}
+                                    onClick={() => handleClick(item.key)}
+                                >
+                                    {item.name}
+                                </ListItem>
+                            )
+                        })
+                    }
+                </List>
+            </div>
+        </Scroll>
+    )
+}
+// 由于基础组件样式较少，直接写在 index.js 中
+const List = styled.div`
+  display: flex;
+  align-items: center;
+  height: 30px;
+  overflow: hidden;
+  >span:first-of-type {
+    display: block;
+    flex: 0 0 auto;
+    padding: 5px 0;
+    margin-right: 5px;
+    color: grey;
+    font-size: ${style["font-size-m"]};
+    vertical-align: middle;
+  }
+`
+const ListItem = styled.span`
+  flex: 0 0 auto;
+  font-size: ${style["font-size-m"]};
+  padding: 5px 8px;
+  border-radius: 10px;
+  &.selected {
+    color: ${style["theme-color"]};
+    border: 1px solid ${style["theme-color"]};
+    opacity: 0.8;
+  }
+`
+
+// 首先考虑接受的参数
+//list 为接受的列表数据
+//oldVal 为当前的 item 值
+//title 为列表左边的标题
+//handleClick 为点击不同的 item 执行的方法
+Horizen.defaultProps = {
+    list: [],
+    oldVal: '',
+    title: '',
+    handleClick: null
+};
+
+Horizen.propTypes = {
+    list: PropTypes.array,
+    oldVal: PropTypes.string,
+    title: PropTypes.string,
+    handleClick: PropTypes.func
+};
+export default memo(Horizen);
