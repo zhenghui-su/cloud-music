8687d0b280d48ccebc4fe1aeba02c3470fd079c5
suzhenghui <343196323@qq.com>
2023-11-03 23:01:53 +0800

    歌手列表功能开发

--- src/api/request.js
+++ src/api/request.js
@@ -7,3 +7,31 @@ export const getBannerRequest = () => {
 export const getRecommendListRequest = () => {
     return axiosInstance.get('/personalized');
 }
+
+export const getHotSingerListRequest = (count) => {
+    return axiosInstance.get(`/top/artists?offset=${count}`);
+}
+export const categoryMap = new Map([
+    ['1001', { type: 1, area: 7 }],
+    ['1002', { type: 2, area: 7 }],
+    ['1003', { type: 3, area: 7 }],
+    ['2001', { type: 1, area: 96 }],
+    ['2002', { type: 2, area: 96 }],
+    ['2003', { type: 3, area: 96 }],
+    ['6001', { type: 1, area: 8 }],
+    ['6002', { type: 2, area: 8 }],
+    ['6003', { type: 3, area: 8 }],
+    ['7001', { type: 1, area: 16 }],
+    ['7002', { type: 2, area: 16 }],
+    ['7003', { type: 3, area: 16 }],
+    ['4001', { type: 1, area: 0 }],
+    ['4002', { type: 2, area: 0 }],
+    ['4003', { type: 3, area: 0 }],
+]);
+export const getSingerListRequest = (category, alpha, count) => {
+    const { type, area } = !!category ? categoryMap.get(category) : {};
+    return axiosInstance.get(
+        `/artist/list?${type && area ? `type=${type}&area=${area}` : ''
+        }&initial=${alpha.toLowerCase()}&offset=${count}`
+    );
+}
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux-immutable';
 import { reducer as recommendReducer } from '../application/Recommend/store/index';
+import { reducer as singersReducer } from '../application/Singers/store/index';
 
 export default combineReducers({
     // 之后开发具体功能模块的时候添加 reducer
     recommend: recommendReducer,
+    singers: singersReducer,
 });
 
